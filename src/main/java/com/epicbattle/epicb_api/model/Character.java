823d763a1fd8f1e659af2d57089db2faa87601d0
KuroKami <grisella.pd@gmail.com>
2024-10-31 20:10:13 +0000

    MySQL connection working.

--- src/main/java/com/epicbattle/epicb_api/model/Character.java
+++ src/main/java/com/epicbattle/epicb_api/model/Character.java
@@ -1,11 +1,9 @@
 package com.epicbattle.epicb_api.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table (name = "characters")
 public class Character {
 
     @Id
