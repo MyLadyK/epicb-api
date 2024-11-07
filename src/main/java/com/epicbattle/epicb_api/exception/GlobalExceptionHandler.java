fdabdb118749bdc93b0ab08252f3a4a8f8a4f2b0
KuroKami <grisella.pd@gmail.com>
2024-11-07 15:42:32 +0000

    Testing CharacterService's working CRUD

--- src/main/java/com/epicbattle/epicb_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/epicbattle/epicb_api/exception/GlobalExceptionHandler.java
@@ -3,9 +3,14 @@ package com.epicbattle.epicb_api.exception;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // Manejar excepciones de tipo ConfigDataResourceNotFoundException
@@ -14,7 +19,7 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>("Personaje no encontrado: " + ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    // Manejar otras excepciones (ejemplo)
+    // Manejar otras excepciones
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGlobalException(Exception ex, WebRequest request) {
         return new ResponseEntity<>("Error inesperado.", HttpStatus.INTERNAL_SERVER_ERROR);
@@ -26,4 +31,16 @@ public class GlobalExceptionHandler {
             super(message);
         }
     }
+
+    /*
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+    }
+     */
 }
--- src/main/java/com/epicbattle/epicb_api/service/CharacterService.java
+++ src/main/java/com/epicbattle/epicb_api/service/CharacterService.java
@@ -1,50 +1,51 @@
-package com.epicbattle.epicb_api.controller;
+package com.epicbattle.epicb_api.service;
 
 import com.epicbattle.epicb_api.exception.GlobalExceptionHandler;
-import com.epicbattle.epicb_api.repository.CharacterRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 import com.epicbattle.epicb_api.model.Character;
-
+import com.epicbattle.epicb_api.repository.CharacterRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping ("/api/characters")
-public class CharacterCrontoller {
+@Service
+public class CharacterService {
 
-    @Autowired
-    private CharacterRepository characterRepository;
+    private final CharacterRepository characterRepository;
+
+    public CharacterService(CharacterRepository characterRepository){
+        this.characterRepository = characterRepository;
+    }
 
     /* Get de siempre
-    @GetMapping
+
     public List <Character> getAllCharacters (){
         return characterRepository.findAll();
     }
     */
 
     // Implementar paginación, lo que solicita resultados en "páginas" y no todos a la vez.
-    @GetMapping
-    public Page<Character> getAllCharacters(Pageable pageable) {
+
+    public Page<com.epicbattle.epicb_api.model.Character> getAllCharacters(Pageable pageable) {
         return characterRepository.findAll(pageable);
     }
 
 
-    @PostMapping
-    public Character createCharacter (@RequestBody Character character){
+
+    public com.epicbattle.epicb_api.model.Character createCharacter (@RequestBody com.epicbattle.epicb_api.model.Character character){
         return characterRepository.save(character);
     }
 
-    @GetMapping("/{id}")
-    public Character getCharacterById(@PathVariable int id) {
+
+    public com.epicbattle.epicb_api.model.Character getCharacterById(@PathVariable int id) {
         return characterRepository.findById(id)
                 .orElseThrow(() -> new GlobalExceptionHandler.ResourceNotFoundException("Personaje no encontrado"));
     }
 
-    @PutMapping("/{id}")
-    public Character updateCharacter(@PathVariable int id, @RequestBody Character characterDetails) {
-        Character character = characterRepository.findById(id)
+
+    public com.epicbattle.epicb_api.model.Character updateCharacter(@PathVariable int id, @RequestBody com.epicbattle.epicb_api.model.Character characterDetails) {
+        com.epicbattle.epicb_api.model.Character character = characterRepository.findById(id)
                 .orElseThrow(() -> new GlobalExceptionHandler.ResourceNotFoundException("Personaje no encontrado"));
 
         character.setNameCharacter(characterDetails.getNameCharacter());
@@ -62,11 +63,10 @@ public class CharacterCrontoller {
         return characterRepository.save(character);
     }
 
-    @DeleteMapping("/{idCharacter}")    //La URL acepta peticiones delete capturando idCharacter desde la ruta
+        //La URL acepta peticiones delete capturando idCharacter desde la ruta
     public ResponseEntity<String> deleteCharacter(@PathVariable int idCharacter) {  // ResponseEntity<String> para devolver el mensaje de personaje eliminado
         Character character = getCharacterById(idCharacter);
         characterRepository.delete(character);
         return ResponseEntity.ok("Personaje eliminado");
     }
-
 }
